3df422a622a415382ace7b1c60b94ebe6bcb3a83
SilverMaiden <h.specials@gmail.com>
2020-01-14 16:45:52 -0800

    Added router with put and get functionality for individual posts

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const postRoutes = require('./postRoutes');
+const singlePostRoutes = require('./singlePostRoutes');
 
 const router = express.Router();
 
 router.use('/posts', postRoutes);
+router.use('/posts', singlePostRoutes)
 
 module.exports = router;
