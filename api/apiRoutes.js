dbcbd26837bd293a66b572bd32b2e6b91e754a37
SilverMaiden <h.specials@gmail.com>
2020-01-14 18:03:16 -0800

    Added post and get functionality for comments

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const postRoutes = require('./postRoutes');
 const singlePostRoutes = require('./singlePostRoutes');
+const commentsRoutes = require('./commentsRoutes');
 
 const router = express.Router();
 
 router.use('/posts', postRoutes);
-router.use('/posts', singlePostRoutes)
+router.use('/posts', singlePostRoutes);
+router.use('/posts', commentsRoutes);
 
 module.exports = router;
